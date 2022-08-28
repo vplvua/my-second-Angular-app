e8db8b855ec738fe963578944364e105dc4a3aad
vasyl.pashko <vasyl.pashko@gmail.com>
2022-08-28 20:54:44 +0300

    configure components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  name = 'My second try learn Angular';
-}
+export class AppComponent {}
